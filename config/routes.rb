7bd95ef3c8db10d4a82f703be5f7ba5371913e11
Pawel Muszynski <pawel.muszynski.95@gmail.com>
2017-06-16 21:54:53 +0200

    Changed createAvailableOffer

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,7 @@ Rails.application.routes.draw do
 
   get 'apply', to: 'applications#new'
 
+  get 'certificate_files' => 'applications#certificate_files'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20170615135459_create_available_offerts.rb
+++ db/migrate/20170615135459_create_available_offerts.rb
@@ -1,4 +1,4 @@
-class CreateAvailableOffers < ActiveRecord::Migration[5.0]
+class CreateAvailableOfferts < ActiveRecord::Migration[5.0]
   def change
     create_table :available_offers do |t|
       t.string :offerName
